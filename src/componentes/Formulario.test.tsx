21bf59b43b7b52748572615333cdcc21d1d7de9c
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-07 11:29:10 -0300

    test: testando o comportamento do form

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,19 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
 
 test('quando o input esta vazio, novos participantes nao podem ser adicionados', () => {
     render(<Formulario />)
-
     // encontrar no DOM o input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+    // encontrar o botao
+    const botao = screen.getByRole('button');
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument();
+    // garantir que o input esteja desabilitado
+    expect(botao).toBeDisabled();
+})
 
+test('adicionar participante caso exista um nome preenchido', () => {
+    render(<Formulario />)
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
     // encontrar o botao
     const botao = screen.getByRole('button');
 
-    // garantir que o input esteja no documento
-    expect(input).toBeInTheDocument();
-    
-    // garantir que o input esteja desabilitado
-    expect(botao).toBeDisabled();
+    // inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Kelly'
+        }
+    });
+
+    // clicar no botao de submeter
+    fireEvent.click(botao)
+
+    // garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+    // garantir que o input nao tenha um valor
+    expect(input).toHaveValue('')
 })

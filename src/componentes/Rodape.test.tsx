09f234968cdc45e71c1a9a29aeeaca4b41ebd993
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-12 11:04:36 -0300

    feat: navegando entre rotas

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -1,16 +1,67 @@
-import { render,screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import Rodape from './Rodape';
+import { useListaDeParticipantes } from 'state/hook/useListaDeParticipantes';
 
-describe('onde nao existem participantes suficientes', () => {
+jest.mock('state/hook/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: jest.fn()
+    }
+})
+
+const mockNavegacao = jest.fn();
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavegacao 
+    }
+})
+
+describe('quando nao existem participantes suficientes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue([]);
+    });
+
     test('a brincadeira nao pode ser iniciada', () => {
         render(
             <RecoilRoot>
                 <Rodape />
             </RecoilRoot>
-        )
+        );
 
-        const botao = screen.getAllByRole('button');
+        const botao = screen.getByRole('button');
         expect(botao).toBeDisabled();
+    });
+});
+
+describe('quando existem participantes suficientes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(['kelly', 'karoline', 'guimarans']);
+    });
+
+    test('a brincadeira pode ser iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+        expect(botao).not.toBeDisabled();
     })
-})
+
+    test('a brincadeira foi iniciada', () => {
+        render(
+            <RecoilRoot>
+                <Rodape />
+            </RecoilRoot>
+        );
+
+        const botao = screen.getByRole('button');
+        fireEvent.click(botao);
+
+        expect(mockNavegacao).toHaveBeenCalledTimes(1);
+        expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
+    })
+});

0c8735ea154e6a2bf3db9f4e6846ffb65743ba12
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-12 13:26:09 -0300

    feat: ajustes

--- src/pages/Sorteio.tsx
+++ src/pages/Sorteio.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { useSetRecoilState } from "recoil";
-import { resultadoDoAmigoSecreto } from "state/atom";
 import { useListaDeParticipantes } from "state/hook/useListaDeParticipantes"
 import { useResultadoDoSorteio } from "state/hook/useResultadoDoSorteio";
+import './Sorteio.css'
 
 export default function Sorteio() {
 
@@ -11,30 +10,35 @@ export default function Sorteio() {
   const [amigoSecreto, setAmigoSecreto] = useState('');
 
   const resultado = useResultadoDoSorteio();
-  
-  function sortear(evento: React.FormEvent<HTMLFormElement> ) {
+
+  function sortear(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
 
-    if(resultado.has(participanteDaVez)) {
+    if (resultado.has(participanteDaVez)) {
       setAmigoSecreto(resultado.get(participanteDaVez)!);
     }
   }
   return (
-    <section>
+    <section className="sorteio">
+      <h2> Quem vai tirar o papelzinho? </h2>
       <form onSubmit={sortear}>
-        <select 
-          required 
-          name="participanteDaVez" 
-          id="participanteDaVez" 
+        <select
+          required
+          name="participanteDaVez"
+          id="participanteDaVez"
           placeholder="Seleciona o seu nome"
           value={participanteDaVez}
           onChange={evento => setParticipanteDaVez(evento.target.value)}
         >
           {participantes.map(participante => <option key={participante}>{participante}</option>)}
         </select>
-        <button> Sortear </button>
+        <p> Clique em sortear para ver quem é seu amigo secreto! </p>
+        <button className="botao-sortear"> Sortear </button>
       </form>
-      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
+      {amigoSecreto && <p className="resultado" role="alert">{amigoSecreto}</p>}
+      <footer className="sorteio">
+        <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+      </footer>
     </section>
   )
 }

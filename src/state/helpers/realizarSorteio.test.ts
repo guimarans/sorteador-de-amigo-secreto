599a5745c8774fe805009116425b04a49ae40eca
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-04-12 12:40:42 -0300

    feat: extraindo a regra de negocio

---
+++ src/state/helpers/realizarSorteio.test.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import { realizarSorteio } from "./realizarSorteio";
+
+describe('dado um sorteio de amigo secreto', () => {
+    test('cada participante não sorteie o próprio nome', () => {
+        const participantes = ['kelly', 'karoline', 'guimarans'];
+
+        const sorteio = realizarSorteio(participantes);
+        participantes.forEach(participante => {
+            const amigoSecreto = sorteio.get(participante);
+
+            expect(amigoSecreto).not.toEqual(participante);
+        })
+    })
+})
--- src/state/hook/useSorteador.ts
+++ src/state/hook/useSorteador.ts
@@ -1,27 +1,13 @@
-import shuffle from "just-shuffle";
 import { useListaDeParticipantes } from "./useListaDeParticipantes"
 import { resultadoDoAmigoSecreto } from "state/atom";
 import { useSetRecoilState } from "recoil";
+import { realizarSorteio } from "state/helpers/realizarSorteio";
 
 export function useSorteador() {
-
   const participantes = useListaDeParticipantes();
   const setResultado = useSetRecoilState(resultadoDoAmigoSecreto);
-
   return () => {
-    
-    const totalDeParticipantes = participantes.length;
-    const embaralhado = shuffle(participantes);
-
-    const resultado = new Map<string, string>();
-
-    for (let index = 0; index < totalDeParticipantes; index++) {
-
-      const indiceDoAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1;
-      resultado.set(embaralhado[index], embaralhado[indiceDoAmigo]);
-
-    }
-
+    const resultado = realizarSorteio(participantes);
     setResultado(resultado);
   }
 }
